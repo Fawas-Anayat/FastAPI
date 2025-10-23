b417ca908a0fc362af77fd4f3b5e6fe6d43b8eb4
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-10-23 20:55:39 +0500

    fastapi code

--- code/main.py
+++ code/main.py
@@ -59,10 +59,10 @@ def load_data(patient_id:str):
     return data[patient_id]
 
 @app.get('/data/{patient_id}')
-def pat_data(patient_id:str):
+def pat_data(patient_id:str=Path(...,title='patient ID',description='this is the uniqu ID assigned to each patient eg(P001,P002 etc)')):
     l=data.keys()
     if patient_id in l:
-        return data[patient_id]
+        return {"patient ID": {patient_id},"patinet name":data[patient_id]}
     raise HTTPException(status_code=404,detail="patient record not found in the database")
 
 @app.get('/informatin')
