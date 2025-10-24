585e04e9d5d5e8ec6a8e88067edade05defed495
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-10-24 18:54:17 +0500

    pydantic

--- code/pydantic_practice.py
+++ code/pydantic_practice.py
@@ -15,3 +15,16 @@ insert_details("usman",34)
 
 """
 
+from pydantic import BaseModel
+
+class patient(BaseModel):  #this is the model means we can say that its the standard and we can use it    
+    name:str
+    age:int
+
+def insert_details(patientt:patient):
+    print(patientt.name)
+    print(patientt.age)
+
+personal_info={'name':'usman','age':34}
+patient1=patient(**personal_info)
+insert_details(patient1)
