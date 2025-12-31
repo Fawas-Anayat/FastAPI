26c6b8afe2f97db8ac5d2df8aa4b9c884db73c68
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-12-31 11:10:22 +0500

    learning fastapi

---
+++ new_code/main.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI ,HTTPException
+from typing import Optional
+from pydantic import basemodel
+
+# Dummy student data for practice
+students = [
+    {
+        "id": 1,
+        "name": "Fawas Anayat",
+        "age": 22,
+        "grade": "A",
+        "city": "Mansehra"
+    },
+    {
+        "id": 2,
+        "name": "Ali Khan",
+        "age": 21,
+        "grade": "B",
+        "city": "Islamabad"
+    },
+    {
+        "id": 3,
+        "name": "Sara Ahmed",
+        "age": 23,
+        "grade": "A",
+        "city": "Lahore"
+    },
+    {
+        "id": 4,
+        "name": "Hassan Ali",
+        "age": 20,
+        "grade": "C",
+        "city": "Karachi"
+    },
+    {
+        "id": 5,
+        "name": "Ayesha Iqbal",
+        "age": 22,
+        "grade": "B",
+        "city": "Peshawar"
+    }
+]
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello FastAPI"}
+
+@app.get('/about')
+def about():
+    return {
+        'name':'Fawas Anayat',
+        'Grade':12,
+        'city':'Mansehra'
+    }
+
+@app.get('/studentData/{student_id}')
+def student_data_by_id(student_id:int):
+    for id in students:
+        if student_id == id['id']:
+            return id
+    raise HTTPException(status_code=404,detail='student not found')
+
+@app.get('/students')
+def students_filtering(name:Optional[str]=None,city:Optional[str]=None,grade:Optional[str]=None):
+    results=students
+    if name:
+        results=[s for s in results if name.lower() in s['name'].lower()]
+    
+    if grade:
+        filtered_students=[]
+        for std in results:
+            if grade.upper() == std['grade'].upper():
+                filtered_students.append(std)
+        results=filtered_students
+
+    if city:
+        results=[ct for ct in results if city.lower() in ct['city'].lower()]
+    return results
+
+class std(basemodel):
+    id:int,
+    name:str,
+    age:int=18,
+    city:str,
+    grade:Optional[str]=None
